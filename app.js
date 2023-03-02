becd6180252106b2a40b3933d6a4d392c7d2c98b
K. A. Vimukthi Shaminda <vimukthishaminda871@gmail.com>
2023-03-02 12:33:41 +0530

    mongodb connected

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
-app.use(express.json)
+const mongoose = require('mongoose')
+app.use(express.json())
+
+const mongo_url ="mongodb+srv://vimukthi:1234pwd@login.tvgmjmp.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(mongo_url,{
+    useNewUrlParser:true
+}).then(()=>{console.log("connect to the db")})
+.catch((e)=>console.log(e))
+
+
 
 app.listen(5000,()=>{
     console.log("Server started");
@@ -8,4 +18,17 @@ app.listen(5000,()=>{
 
 app.post("/post",async(req,res)=>{
     console.log(req.body);
+    const {data} = req.body;
+
+try {
+    if(data1 == "vimukthi"){
+        res.send({status:"ok"});
+    }else{
+        res.send({status:"User Not Found."});
+    }
+} catch (error) {
+    res.send({status:"error"});
+}
+
+    
 })
